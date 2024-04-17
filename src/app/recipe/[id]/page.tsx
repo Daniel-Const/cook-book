398dbc09cbd53f3052b66f08213cb993bf5faa08
Daniel Constantinidis <dancon98@hotmail.com>
2024-04-17 19:06:55 +1000

    add more to recipe page

--- src/app/recipe/[id]/page.tsx
+++ src/app/recipe/[id]/page.tsx
@@ -1,17 +1,26 @@
-'use client';
-
+"use client";
 /**
  * Recipe page
  */
 
 export default function Page({ params }: { params: { id: string } }) {
-    return (
-        <main className="flex min-h-screen flex-col items-center justify-between p-24">
-            <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-                <div className="grid p-2">
-                    <p> You're on the {params.id} recipe</p>
-                </div>
-            </div>
-        </main>
-    );
+  // TODO: Fetch recipe from server by id
+  const recipe = {
+    title: "Ravioli",
+    description:
+      "Easy to cook! Simple and effective. Ravioli with some tomato sauce will always be a great option",
+    ingredients: [
+      { name: "ravioli-pasta", quantity: "1 packet" },
+      { name: "Barilla basil sauce", quantity: "1 jar" },
+    ],
+  };
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm">
+        <h1 className="text-6xl">{recipe.title}</h1>
+        <p className="pt-10">{recipe.description}</p>
+        <h2 className="text-4xl pt-10">Ingredients</h2>
+      </div>
+    </main>
+  );
 }
