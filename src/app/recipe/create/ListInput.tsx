a311e6780a0fd11e5d4fdc88facd3b316047adfe
Daniel-Const <dancon98@hotmail.com>
2024-07-02 21:32:03 +1000

    - add list input component - extend create recipe page

---
+++ src/app/recipe/create/ListInput.tsx
@@ -0,0 +1,29 @@
+import { useState } from "react"
+
+export default function ListInput() {
+
+    const [inputs, setInputs] = useState(["1"])
+
+    function extendList() {
+        setInputs((currInputs) => [
+            ...currInputs,
+            (currInputs.length + 1).toString()
+        ])
+    }
+
+    return (
+        <>
+        <ul>
+            {inputs.map((key, index) => (
+                <li key={index}>
+                    <div className="pb-2">
+                        <input className="pl-2 text-black" type="text" name={"list-"+index} id={key} placeholder={key} />
+                    </div>
+                </li>
+            ))}
+        </ul>
+        <br />
+        <button className="rounded-full outline p-2" onClick={extendList}>Add</button>
+        </>
+    )
+}
--- src/app/recipe/create/page.tsx
+++ src/app/recipe/create/page.tsx
@@ -1,9 +1,56 @@
+'use client'
+
+import { FormEvent } from 'react'
+
 import Layout from "@/components/layout/Layout"
+import ListInput from './ListInput'
 
 export default function CreateRecipe() {
+
+    async function onSubmit(event: FormEvent<HTMLFormElement>) {
+        event.preventDefault()
+        const formData = new FormData(event.currentTarget)
+        const title = formData.get("title")
+
+        const ingredients = []
+        let value = formData.get("list-0")
+        let i = 1
+        while (value != null) {
+            ingredients.push(value)
+            value = formData.get(`list-${i}`)
+            i++
+        }
+
+        const method = formData.get("method")
+        
+        // TODO: API Request to save recipe
+        console.log({title, ingredients, method})
+    }
+
     return (
         <Layout>
-            <h1>Add a recipe!</h1>
+            <h1 className="text-2xl">Add a recipe!</h1>
+            <form onSubmit={onSubmit}>
+                <div className="flex-row">
+                    <div className="mt-8">
+                        <p>Title</p>
+                        <input className="text-black pl-2" type="text" id="title" name="title" />
+                    </div>
+                    <div className="mt-8">
+                       <p>Ingredients</p>
+                       <ListInput />
+                    </div>
+                    <div className="mt-8">
+                        <p>Method</p>
+                        <textarea className="text-black pl-2" id="method" name="method" rows={8} cols={40} />
+                    </div>
+                    <div className="text-center mt-8" >
+                        <button className="px-4 py-2 rounded-full bg-blue-500 hover:bg-blue-300" type="submit">
+                            Save
+                        </button>
+                    </div>
+                </div>
+            </form>
         </Layout>
     )    
 }
