8c2258e9c86252b7f6f6def2f578940eb9f841ee
Daniel-Const <dancon98@hotmail.com>
2025-01-14 15:16:44 +1100

    - make layout server component - move useContext for alerts to Alert component

--- src/components/layout/Alert.tsx
+++ src/components/layout/Alert.tsx
@@ -1,17 +1,22 @@
-import { AlertType } from "@/context/AlertContext";
+'use client';
 
-export function Alert({ type, message }: { type: AlertType, message: string }) {
-    let alertStyle = ""
-    switch (type) {
+import { AlertType } from '@/context/AlertContext';
+import { useContext } from 'react';
+import { AlertContext } from '@/context/AlertContext';
+
+export function Alert() {
+    const alertCtx = useContext(AlertContext);
+
+    let alertStyle = '';
+    switch (alertCtx?.type) {
         case AlertType.Info:
-            alertStyle = "bg-green-50 dark:bg-gray-800 dark:text-green-400"
+            alertStyle = 'bg-green-50 dark:bg-gray-800 dark:text-green-400';
             break;
     }
 
-    return (
-        <div className={"p-4 text-sm rounded-lg " + alertStyle}>
-            {message}
-        </div>
-    )
+    return alertCtx?.show ? (
+        <div className={'p-4 text-sm rounded-lg ' + alertStyle}>{alertCtx?.message}</div>
+    ) : (
+        ''
+    );
 }
-
--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,24 +1,15 @@
-'use client';
-
-import { useContext } from 'react';
 import { Alert } from './Alert';
 import { MenuButton } from './MenuButton';
-import { AlertContext } from '@/context/AlertContext';
 
 export default function Layout({
     children,
     showBack = true
 }: Readonly<{ children: React.ReactNode; showBack?: boolean }>) {
-    const alertCtx = useContext(AlertContext);
     return (
         <div>
             <div className="flex">
                 <div className="absolute top-0 right-0 mt-8 mr-8 flex mx-auto items-center">
-                    {alertCtx?.show ? (
-                        <Alert type={alertCtx.type} message={alertCtx.message} />
-                    ) : (
-                        ''
-                    )}
+                    <Alert />
                 </div>
             </div>
 
